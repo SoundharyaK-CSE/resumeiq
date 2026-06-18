b626f25bd72db0b2335b42f32bbbb403d796433d
sou1198 <240251.cs@rmkec.ac.in>
2026-06-18 20:01:49 +0530

    Update API to use live backend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://resumeiq-backend-artu.onrender.com/api',
 })
 
 export default api
